7588772b087c753008ce671354bac07aeb759e85
Роберт <pobept46.2002@gmail.com>
2024-05-20 11:56:25 +0500

    update workflow #2

--- meta.py
+++ meta.py
@@ -3,7 +3,7 @@ import glob
 
 ipa = glob.glob('*.ipa')[0]
 
-proc = subprocess.Popen('docker','run','--rm','-e',f'filename={str(ipa)}','-v','/tmp/ipas/:/home/work/files','ipax:latest'], stdout=subprocess.PIPE, shell=True)
+proc = subprocess.Popen(['docker','run','--rm','-e',f'filename={str(ipa)}','-v','/tmp/ipas/:/home/work/files','ipax:latest'], stdout=subprocess.PIPE, shell=True)
 (out, err) = proc.communicate()
 
 with open("res.txt","w") as f:
